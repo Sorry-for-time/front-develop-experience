98e1776d7812a119884bc5854eb5df781bd02a19
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-09-11 11:25:45 +0800

    挂载全局 loading-bar 组件

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import "@/assets/main.scss";
 import "@/assets/Transition.scss";
-import "@/assets/art-text.scss"
-;
+import "@/assets/art-text.scss";
+import "@/assets/main.scss";
+
+import { useLoadingBar } from "@/hooks/useLoadingBar";
+
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 
@@ -9,3 +11,6 @@ import App from "@/App.vue";
 import { router } from "@/router/router";
 
 createApp(App).use(router).use(createPinia()).mount("#app");
+const loadingBarHooks = useLoadingBar(document.body);
+
+export { loadingBarHooks };
