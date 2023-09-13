27f1803f6f7936c0841ba83f56147db355d8d72b
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-09-13 10:38:00 +0800

    配置路由错误处理 & 设置路由未匹配错误加载条形式

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from "vue-router";
-import { sampleRouteConfig } from "@/router/routes/sample";
-import { indexConfig } from "@/router/routes/index";
 import { loadingBarHooks } from "@/main";
+import { indexConfig } from "@/router/routes/index";
+import { sampleRouteConfig } from "@/router/routes/sample";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,8 +22,18 @@ router.beforeEach((to, from, next) => {
   loadingBarHooks.start();
   next();
 });
+
+router.onError((reason, to, from) => {
+  router.replace({
+    name: "not-found"
+  });
+  console.warn(reason, `to: ${to.fullPath}, from: ${from.fullPath}`);
+});
+
 router.afterEach((to, from, failure) => {
-  if (failure) {
+  if (to.name === "not-found") {
+    loadingBarHooks.error();
+  } else if (failure) {
     if (failure.message.startsWith("Avoided redundant navigation")) {
       loadingBarHooks.finish();
     } else {
