dcd730b987e94e0c71a35f35ab193aeb0d84429e
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-09-14 19:50:47 +0800

    通过 webWorker + offscreenCanvas 解决在主线程计算渲染动态 canvas 背景导致的路由切换卡顿以及 UI 更新阻塞问题

--- src/components/canvasBackground/CanvasBackground.tsx
+++ src/components/canvasBackground/CanvasBackground.tsx
@@ -1,12 +1,13 @@
-import { LineScrollEffect } from "@/effect/LineScrollEffect";
 import { defineAsyncComponent } from "vue";
 import { Component, Vue, toNative } from "vue-facing-decorator";
 import style from "./CanvasBackground.module.scss";
+import type { InitConfig } from "./CanvasBackground.worker";
+import BackgroundAnimationWorker from "./CanvasBackground.worker?worker";
 
 const CanvasBackgroundRender = (that: CanvasBackgroundBase): JSX.Element => {
   return (
-    <div ref="wrapper" class={style["background-canvas-wrapper"]}>
-      <div class={style["canvas-wrapper"]}>
+    <div class={style["background-canvas-wrapper"]}>
+      <div ref="wrapper" class={style["canvas-wrapper"]}>
         <canvas ref="canvas" />
       </div>
     </div>
@@ -22,44 +23,59 @@ const CanvasBackgroundRender = (that: CanvasBackgroundBase): JSX.Element => {
 })
 class CanvasBackgroundBase extends Vue {
   /**
-   * 监听器 id
+   * 监听器回调记录
    */
-  private listenerFd: number | undefined = void 0;
-  private lineScrollEffect!: LineScrollEffect;
+  private functor: (() => void) | undefined;
+  /**
+   * 专用工背景绘制工作者线程
+   */
+  private worker: Worker | undefined = void 0;
 
   public mounted(): void {
     console.log("mounted!");
-    const canvas = this.$refs.canvas as HTMLCanvasElement;
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    const originCanvas = this.$refs.canvas as HTMLCanvasElement;
     const wrapper = this.$refs.wrapper as HTMLDivElement;
-    const offsetCanvas = canvas.transferControlToOffscreen();
 
-    const offsetScreenCtx: OffscreenCanvasRenderingContext2D =
-      offsetCanvas.getContext("2d", {
-        willReadFrequently: true
-      })!;
+    const { clientWidth, clientHeight } = wrapper;
+    originCanvas.width = clientWidth;
+    originCanvas.height = clientHeight;
 
-    // canvas settings
-    offsetScreenCtx.fillStyle = "white";
-    offsetScreenCtx.strokeStyle = "white";
-    offsetScreenCtx.lineWidth = 1;
-    this.lineScrollEffect = new LineScrollEffect(offsetCanvas, wrapper);
-    this.lineScrollEffect.uiResizeAutoFit = true;
+    const offsetCanvas: OffscreenCanvas =
+      originCanvas.transferControlToOffscreen();
 
-    const animate = (): void => {
-      offsetScreenCtx.clearRect(0, 0, canvas.width, canvas.height);
-      this.lineScrollEffect.render(offsetScreenCtx);
-      this.listenerFd = requestAnimationFrame(animate);
+    const threadMsgConfig: InitConfig = {
+      canvas: offsetCanvas,
+      width: wrapper.clientWidth,
+      height: wrapper.clientHeight
     };
-    animate();
+
+    this.worker = new BackgroundAnimationWorker();
+    // 将离屏绘制画布传递给工作者线程
+    this.worker.postMessage(threadMsgConfig, {
+      transfer: [offsetCanvas]
+    });
+    setTimeout(() => {
+      console.log(offsetCanvas);
+    }, 1000);
+
+    this.functor = (): void => {
+      const { clientWidth, clientHeight } = wrapper;
+      this.worker!.postMessage({
+        width: clientWidth,
+        height: clientHeight
+      });
+    };
+    window.visualViewport!.addEventListener("resize", this.functor);
   }
 
   public beforeUnmount(): void {
-    // clean up
-    if (typeof this.listenerFd === "number") {
-      window.cancelAnimationFrame(this.listenerFd);
+    // 清理监听器
+    if (this.functor) {
+      window.visualViewport!.removeEventListener("resize", this.functor);
     }
+    // 清理线程
+    this.worker?.terminate();
+    console.log("terminate thread-worker!");
   }
 }
 
