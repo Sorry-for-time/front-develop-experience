c356e0479adbd2787a82ac5d1e5ae4d83c21766e
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-09-14 19:53:03 +0800

    添加在 vue-class 组件下使用的方法节流以及防抖装饰器

---
+++ src/decorators/performanceUtil.ts
@@ -0,0 +1,92 @@
+import { createDecorator } from "vue-facing-decorator";
+
+/**
+ * Vue-Class 组件防抖装饰器(这要求方法没有返回值)
+ *
+ * @param wait 间隔延迟
+ * @param startImmediate 首次使用是否立即执行
+ */
+function UseDebounce(
+  wait: Readonly<number> = 200,
+  startImmediate: Readonly<boolean> = true
+): MethodDecorator {
+  let timer: number | undefined = void 0;
+  let immediate: boolean = startImmediate;
+
+  return createDecorator(
+    (options, key) => {
+      const originMethod: Function | undefined = options.methods?.[key];
+      if (typeof originMethod !== "function") {
+        throw new TypeError(`${key} is not a method`);
+      } else {
+        options.methods[key] = new Proxy(originMethod!, {
+          apply(target, thisArg, argArray) {
+            if (immediate) {
+              timer = setTimeout(() => {
+                Reflect.apply(target, thisArg, argArray);
+              }, 0);
+              immediate = false;
+            } else {
+              if (timer) {
+                clearTimeout(timer);
+                timer = setTimeout(() => {
+                  Reflect.apply(target, thisArg, argArray);
+                }, wait);
+              }
+            }
+          }
+        });
+      }
+    },
+    {
+      preserve: true
+    }
+  ) as MethodDecorator;
+}
+
+/**
+ * Vue-Class 组件方法节流装饰器(这要求方法没有返回值)
+ *
+ * @param wait 间隔延迟
+ * @param startImmediate 首次使用是否立即执行
+ */
+function UseThrottle(
+  wait: Readonly<number> = 200,
+  startImmediate: Readonly<boolean> = true
+): MethodDecorator {
+  let locker: boolean = false;
+  let immediate: boolean = startImmediate;
+
+  return createDecorator(
+    (options, key) => {
+      const originMethod: Function | undefined = options.methods?.[key];
+      if (typeof originMethod !== "function") {
+        throw new TypeError(`${key} is not a method`);
+      } else {
+        options.methods[key] = new Proxy(originMethod!, {
+          apply(target, thisArg, argArray) {
+            if (immediate) {
+              setTimeout(() => {
+                Reflect.apply(target, thisArg, argArray);
+              }, 0);
+              immediate = false;
+            }
+            if (locker) {
+              return;
+            }
+            locker = true;
+            setTimeout((): void => {
+              Reflect.apply(target, thisArg, argArray);
+              locker = false;
+            }, wait);
+          }
+        });
+      }
+    },
+    {
+      preserve: true
+    }
+  ) as MethodDecorator;
+}
+
+export { UseDebounce, UseThrottle };
