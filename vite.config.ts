59b6cf8575ba2f477756d6cc7400f06d27ef5efb
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-09-14 15:26:52 +0800

    添加 jsx 模板装饰器支持

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,18 @@ import { fileURLToPath, URL } from "node:url";
 
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import { defineConfig } from "vite";
 import autoprefixer from "autoprefixer";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx({
+      // tsx/jsx 模板添加装饰器语法支持
+      babelPlugins: [["@babel/plugin-proposal-decorators", { legacy: true }]]
+    })
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
