04d09e9ebcf902c76fed0f6725ff9f60bf6cfdcc
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-09-17 13:10:04 +0800

    添加复复杂嵌套数据结构以测试 pinia-indexedDB 存储插件正常解析&写入功能

--- src/stores/useListStore.ts
+++ src/stores/useListStore.ts
@@ -1,12 +1,42 @@
 import { defineStore } from "pinia";
+import {
+  computed,
+  reactive,
+  readonly,
+  ref,
+  shallowReadonly,
+  shallowRef
+} from "vue";
 import { StoreIdEnum } from "./StoreIdEnum";
 
-const useListStore = defineStore(StoreIdEnum.LIST, {
-  state() {
-    return {
-      list: [1, 2, 3, 4]
-    };
-  }
+const useListStore = defineStore(StoreIdEnum.LIST, () => {
+  const list = ref<Array<number>>([]);
+  const updateList = () => [];
+  const listComputed = computed(() => list.value);
+  return {
+    list,
+    listComputed,
+    updateList,
+    deepRef: ref({
+      list: [
+        [1, 2, 3],
+        ["a", "bc"],
+        shallowRef([12, 222]),
+        readonly({ some: "readonly" }),
+        shallowReadonly({ p: 233 }),
+        reactive([
+          1,
+          2,
+          3,
+          shallowReadonly({
+            xxx: "fff"
+          })
+        ]),
+        readonly({ num: 23 }),
+        ref("1")
+      ]
+    })
+  };
 });
 
 export { useListStore };
