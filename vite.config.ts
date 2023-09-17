ba4bb7b43a216b9f737b8d41e116f0cccdd9aad3
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-09-17 12:47:58 +0800

    配置 https 插件

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import autoprefixer from "autoprefixer";
 import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,11 @@ export default defineConfig({
     vueJsx({
       // tsx/jsx 模板添加装饰器语法支持
       babelPlugins: [["@babel/plugin-proposal-decorators", { legacy: true }]]
+    }),
+    mkcert({
+      force: true,
+      autoUpgrade: true,
+      source: "coding"
     })
   ],
   resolve: {
@@ -28,7 +34,13 @@ export default defineConfig({
   },
   server: {
     port: 3002,
-    open: true
+    open: true,
+    https: true,
+    // 为启用 SharedArrayBuffer 添加头部信息
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp"
+    }
   },
   css: {
     postcss: {
