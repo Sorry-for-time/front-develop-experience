c583780b569abb714803f41da6ba1454a706bc9a
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-09-17 13:07:58 +0800

    添加定型数组(Uint8ClampedArray) store

---
+++ src/stores/useCanvasImageStore.ts
@@ -0,0 +1,31 @@
+import { defineStore } from "pinia";
+import { ref, shallowRef, type Ref, type ShallowRef } from "vue";
+import { StoreIdEnum } from "./StoreIdEnum";
+
+const useCanvasImageStore = defineStore(StoreIdEnum.CANVAS_IMAGE, () => {
+  const width: Ref<number> = ref(0);
+  const height: Ref<number> = ref(0);
+  const imageData: ShallowRef<Uint8ClampedArray | undefined> = shallowRef(
+    void 0
+  );
+
+  const updateImage = (
+    imageBuffer: Uint8ClampedArray,
+    w: number,
+    h: number
+  ): void => {
+    imageData.value = imageBuffer;
+    width.value = w;
+    height.value = h;
+    console.log("update success");
+  };
+
+  return {
+    width,
+    height,
+    imageData,
+    updateImage
+  };
+});
+
+export { useCanvasImageStore };
