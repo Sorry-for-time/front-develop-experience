9d460067b41fd431b16068cd3e9ccd62ba766e4a
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-09-18 16:51:17 +0800

    使用信号量重构响应式变量

--- src/components/capitalHintInput/CapitalHintInput.tsx
+++ src/components/capitalHintInput/CapitalHintInput.tsx
@@ -1,4 +1,5 @@
-import { Transition, defineComponent, ref, type PropType, type Ref } from "vue";
+import { createSignal } from "@/util/signal";
+import { Transition, defineComponent, type PropType } from "vue";
 import style from "./CapitalHintInput.module.scss";
 
 /**
@@ -27,13 +28,17 @@ const CapitalHintInput = defineComponent({
   emits: ["update:modelValue" /* 定义更新函数派发事件 */],
   setup() {
     /**
-     * 输入框是否处于输入状态
+     * 输入框输入状态
      */
-    const inputFocus: Ref<boolean> = ref(false);
+    const [focusState, updateFocusState] = createSignal(false, {
+      equals: false
+    });
     /**
-     * 当前键盘大写锁定是否打开
+     * 当前键盘大写锁定状态
      */
-    const isCapital: Ref<boolean> = ref(false);
+    const [capitalState, updateCapitalState] = createSignal(false, {
+      equals: false
+    });
     /**
      * 判断一开始的键盘情况是否已经为开启了大写键盘的情况, 如果在进行输入(键盘事件发生)之前大写锁定已打开,
      * 那么更新大写提示控制变量为显示, 如果大写未锁定, 那么更新大写提示控制变量为隐藏
@@ -42,8 +47,8 @@ const CapitalHintInput = defineComponent({
      */
     function judgeDefaultCapitalStatus(ev: PointerEvent) {
       ev.getModifierState("CapsLock")
-        ? (isCapital.value = true)
-        : (isCapital.value = false);
+        ? updateCapitalState(true)
+        : updateCapitalState(false);
     }
     /**
      * 在键盘进行输入事件时判断大写锁定情况
@@ -52,23 +57,35 @@ const CapitalHintInput = defineComponent({
      */
     function inputHandler(ev: KeyboardEvent) {
       ev.getModifierState("CapsLock")
-        ? (isCapital.value = true)
-        : (isCapital.value = false);
+        ? updateCapitalState(true)
+        : updateCapitalState(false);
     }
     return {
-      inputFocus,
-      isCapital,
+      focusState,
+      updateFocusState,
+      capitalState,
       doKeyup: inputHandler,
       doPointerDown: judgeDefaultCapitalStatus
     };
   },
   render(): JSX.Element {
+    const {
+      type,
+      modelValue,
+      $emit,
+      focusState,
+      capitalState,
+      updateFocusState,
+      doKeyup,
+      doPointerDown
+    } = this;
+
     return (
       <div class={style["input-wrapper"]}>
         <input
           class={style["input-ref"]}
-          type={this.type}
-          value={this.modelValue}
+          type={type}
+          value={modelValue}
           onInput={(payload: Event) => {
             let val = (payload.target as HTMLInputElement).value;
             if ((this.modelValueModifiers as any).trim) {
@@ -78,17 +95,17 @@ const CapitalHintInput = defineComponent({
               val = val.toUpperCase();
             }
             // 派发同步更新
-            this.$emit("update:modelValue", val);
+            $emit("update:modelValue", val);
           }}
           placeholder="please input some text"
-          onFocusin={() => (this.inputFocus = true)}
-          onFocusout={() => (this.inputFocus = false)}
-          onKeyup={this.doKeyup}
-          onPointerdown={this.doPointerDown}
+          onFocusin={() => updateFocusState(true)}
+          onFocusout={() => updateFocusState(false)}
+          onKeyup={doKeyup}
+          onPointerdown={doPointerDown}
         />
         <Transition name="bounce">
           <span
-            v-show={this.inputFocus && this.isCapital}
+            v-show={focusState() && capitalState()}
             class={style["capital-hint"]}
           >
             大写锁定已打开
