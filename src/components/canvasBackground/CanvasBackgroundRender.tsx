0f3f584a8f44f44ffac071bb94fa492304901b80
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-09-18 14:30:19 +0800

    去除未使用的变量

--- src/components/canvasBackground/CanvasBackgroundRender.tsx
+++ src/components/canvasBackground/CanvasBackgroundRender.tsx
@@ -1,9 +1,6 @@
-import type { CanvasBackgroundBase } from "./CanvasBackground";
 import style from "./CanvasBackground.module.scss";
 
-export const CanvasBackgroundRender = (
-  that: CanvasBackgroundBase
-): JSX.Element => {
+export const CanvasBackgroundRender = (): JSX.Element => {
   return (
     <div class={style["background-canvas-wrapper"]}>
       <div ref="wrapper" class={style["canvas-wrapper"]}>
