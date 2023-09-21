3cdbdcffdace9db765d513a4769c7332dcf3bd6c
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-09-21 10:16:48 +0800

    更新指令注册

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import "@/assets/style/main.scss";
 
-import { vDraggable } from "@/directives/vDraggable";
-import { useLoadingBar } from "@/hooks/useLoadingBar";
-
 import { App } from "@/App";
 import { createPiniaIndexedDBPersistPlugin } from "@/plugin/piniaPlugin/piniaPersistPlugin";
 import { router } from "@/router/router";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 
+import { vMoveable } from "@/directives/vMoveable";
+import { useLoadingBar } from "@/hooks/useLoadingBar";
+
 const { plugin, pluginStatus } = createPiniaIndexedDBPersistPlugin(2, true);
 
 createApp(App)
-  .directive("draggable", vDraggable)
+  .directive("moveable", vMoveable) // register global custom directive command
   .use(router)
   .use(createPinia().use(plugin))
   .mount("#app");
