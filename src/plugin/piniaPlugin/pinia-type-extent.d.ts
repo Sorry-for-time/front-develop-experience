a97079f609b6c4df962231e6c3d43ff8f6e1c6b0
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-09-28 19:07:52 +0800

    拓展可选配置

--- src/plugin/piniaPlugin/pinia-type-extent.d.ts
+++ src/plugin/piniaPlugin/pinia-type-extent.d.ts
@@ -5,16 +5,31 @@ declare module "pinia" {
     /**
      * 持久化配置
      */
-    persist?: Partial<{
+    persist?: {
       /**
        * 持久化类型, 仅 `indexedDB` 存储支持 web-worker 上下文环境
        */
       storage: "indexedDB" | "sessionStorage" | "localStorage";
+    } & Partial<{
       /**
        * 是否持久化只读数据(对于 readonly, shallowReadOnly...)
        * @see {@link https://cn.vuejs.org/api/reactivity-advanced.html#shallowreadonly}
        */
       persistReadonly: boolean;
+      /**
+       * 自定义数据存储库的名称, 默认为 store.$id
+       */
+      key: string;
+      /**
+       * 加载恢复本地数据前的操作
+       * @param ctx pinia 插件上下文
+       */
+      beforeRestore: (ctx: PiniaPluginContext) => void;
+      /**
+       * 从本地恢复数据后的操作
+       * @param ctx pinia 插件上下文
+       */
+      afterRestore: (ctx: PiniaPluginContext) => void;
     }>;
   }
 }
