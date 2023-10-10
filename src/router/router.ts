4ba4f1b47ab6a364016438584b97a5adebb68ae5
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-10-10 09:54:09 +0800

    未使用变量添加下划线前缀以忽略

--- src/router/router.ts
+++ src/router/router.ts
@@ -5,8 +5,7 @@ import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [...indexConfig],
-  strict: true,
-  scrollBehavior: (to, from, savedPosition) => {
+  scrollBehavior: (_to, _from, savedPosition) => {
     return savedPosition
       ? savedPosition
       : {
